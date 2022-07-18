b72d3e5cba2c46a06df923766b941bde3a228541
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-07-18 17:32:43 +0300

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+import React from "react";
+function keypad() {
+    function handle() {
+        console.log('Entering password...')
+    }
+    return(
+        <input type="password" onChange={handle}></input>
+        )
+}
+export default keypad;
